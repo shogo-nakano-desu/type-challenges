fe9a1a808eae615838d4271115e40ca455f96c4b
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2021-11-16 23:25:43 +0900

    add concat

--- answers/easy/easy-if-268.ts
+++ answers/easy/easy-if-268.ts
@@ -2,22 +2,21 @@
   268 - If
   -------
   by Pavel Glushkov (@pashutk) #easy #utils
-
   ### Question
   Implement a utils `If` which accepts condition `C`, a truthy return type `T`, and a falsy return type `F`. `C` is expected to be either `true` or `false` while `T` and `F` can be any type.
   For example:
-
   ```ts
   type A = If<true, 'a', 'b'>  // expected to be 'a'
   type B = If<false, 'a', 'b'> // expected to be 'b'
   ```
-
   > View on GitHub: https://tsch.js.org/268
 */
-
 /* _____________ Your Code Here _____________ */
 
-type If<C, T, F> = any;
+// myfirst answer
+type If<C extends boolean, T, F> = C extends true ? T : F;
+
+// correct answer
 
 /* _____________ Test Cases _____________ */
 import { Equal, Expect } from "@type-challenges/utils";
