571c868ba64b5cb4f9621a35205c91b6c75aef32
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-05-29 19:44:21 +0900

    refactor: add comment

--- answers/easy/00011-tuple-to-object.ts
+++ answers/easy/00011-tuple-to-object.ts
@@ -20,8 +20,10 @@
 
 /* _____________ Your Code Here _____________ */
 
-type TupleToObject<T extends readonly (keyof any)[]> = { [t in T[number]]: t };
-// type TupleToObject<T extends readonly any[]> = { [t in T[number]]: t };
+//keyof any === number|string|symbol
+type TupleToObject<T extends readonly (keyof any)[]> = {
+  [t in T[number]]: t;
+};
 
 /* _____________ Test Cases _____________ */
 import { Equal, Expect } from "@type-challenges/utils";
